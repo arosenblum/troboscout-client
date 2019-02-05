391ae71fa7c41a18f26abed9d8a7d69c056d723b
bmongar <bmongar@keyholesoftware.com>
2019-02-05 13:06:16 -0600

    enable/disable

--- src/app/event/event.component.ts
+++ src/app/event/event.component.ts
@@ -12,8 +12,13 @@ import axios, { AxiosRequestConfig, AxiosPromise } from 'axios';
 })
 export class EventComponent implements OnInit {
   eventList: Array<Event> = [];
+  successes: Array<Event> = [];
+  failures: Array<Event> = [];
   waitPeriod: number;
   timer: any;
+  lastCorrelationId: Date;
+  hasPiece :boolean = false;
+  piece : string;
 
   eventForm = new FormGroup({
     scouter: new FormControl(''),
@@ -42,28 +47,27 @@ export class EventComponent implements OnInit {
   sendEvents () {
     console.log('Looking for events to send: ' + new Date());
     let total = this.eventList.length;
-    let failures = [];
-    let successes = [];
+    let failures = this.failures;
+    let successes = this.successes;
     let self = this;
+    
 
     // iterate events, sending each to the server
     if (total > 0) {
-      for (let aScoutingEvent of this.eventList) {
+      for (var i = 0; i < this.eventList.length; i++){
+        let aScoutingEvent = this.eventList[i];
+
         axios.post('http://trobots5013.com:8080/event', aScoutingEvent).then(function (response) {
           successes.push(aScoutingEvent);
+          self.eventList = self.eventList.splice(i--,1);
           console.log(response);
-          if (failures.length + successes.length === total) {
-            // TODO - done now, clear the timeout, and cleanup, or something
-            clearTimeout(self.timer);
-          }
         }).catch(function (error) {
           failures.push(aScoutingEvent);
+          self.eventList = self.eventList.splice(i--,1);
           console.log(error);
-          if (failures.length + successes.length === total) {
-            // TODO - done now, clear the timeout, and cleanup, or something
-            clearTimeout(self.timer);
-          }
         });
+        console.log('event list size = ' + this.eventList.length);
+        this.doWork();
       }
     } else {
       this.doWork();
@@ -76,6 +80,16 @@ export class EventComponent implements OnInit {
     scoutingEvent.team = this.eventForm.get('team').value;
     scoutingEvent.round = this.eventForm.get('round').value;
     scoutingEvent.eventTimestamp = new Date();
+    if (scoutingEvent.action == 'take'){
+    	scoutingEvent.correlationId = scoutingEvent.eventTimestamp;
+    	this.lastCorrelationId = scoutingEvent.eventTimestamp;
+    	this.hasPiece=true;
+    	this.piece = scoutingEvent.piece;
+    } else {
+    	scoutingEvent.correlationId = this.lastCorrelationId;
+    	this.hasPiece=false;
+    	this.piece = null;
+    }
     this.eventList.push(scoutingEvent);
   }
 
@@ -87,12 +101,23 @@ export class EventComponent implements OnInit {
   }
 
   makeEvent(piece,action, location) {
-    console.warn("cargoFloor");
+  	if (action == 'take' && this.hasPiece) return;
+  	if ((action == 'place' || action == 'fail') && !this.hasPiece) return;
+  	if (this.hasPiece && this.piece != piece) return;
     let scoutingEvent = new ScoutEvent();
     scoutingEvent.piece = piece;
     scoutingEvent.action = action;
     scoutingEvent.location = location;
     this.saveEvent(scoutingEvent);
+  }
+   makeEventDrop() {
+   	if (this.hasPiece){
+	    let scoutingEvent = new ScoutEvent();
+	    scoutingEvent.piece = this.piece;
+	    scoutingEvent.action = 'drop';
+	    scoutingEvent.location = 'drop';
+	    this.saveEvent(scoutingEvent);
+    }
   }
   getEventList() {
     return this.eventList;
@@ -101,5 +126,23 @@ export class EventComponent implements OnInit {
   getTimer() {
   	return this.timer;
   }
-
+  
+  getHasPiece() {
+  	return this.hasPiece;
+  }
+  
+  hasCargo(){
+  	return this.hasPiece && this.piece == 'cargo';
+  }
+  
+    
+  hasHatch(){
+  	return this.hasPiece && this.piece == 'hatch';
+  }
+  retryFailures(){
+  	for (var i = 0; i < this.failures.length; i++){
+  		this.eventList.push(this.failures[i]);
+  	}
+  	this.failures.splice(0,this.failures.length);
+  }
 }
